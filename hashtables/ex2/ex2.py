ea5ac62ec33c51414d71c0f313ad6cada66c9d8f
Melvin Merlos <melvinamerlos@gmail.com>
2019-10-16 21:03:24 -0400

    miners, ex1, ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -20,4 +20,16 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
 
+    # insert tickets into hash table
+    for i in range(length):
+        hash_table_insert(ht, tickets[i].source, tickets[i].destination)
+
+    # setting the first destination to starting point's destination
+    route[0] = hash_table_retrieve(ht, 'NONE')
+
+    # after starting point set, loop thru route list and set next destination in list
+    for i in range(1, length):
+        route[i] = hash_table_retrieve(ht, route[i - 1])
+
+    # return the list of routes
     return route
